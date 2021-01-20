bfce6d16e770311784466dd4744242e1f755962c
Cristian <crislm1322@gmail.com>
2021-01-20 15:40:18 -0500

    working in TheResources component

---
+++ Section 10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <the-header title="RememberMe"></the-header>
+    <the-resources title="RememberMe"></the-resources>
+  </div>
+</template>
+
+<script>
+// local components
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources';
+
+export default {
+  components: {
+    TheHeader,
+    TheResources,
+  },
+};
+</script>,
+    TheResources
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+button {
+  cursor: pointer;
+}
+</style>
---
+++ Section 10/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul v-if="resources">
+    <learning-resources
+      v-for="resource in resources"
+      :key="resource.id"
+      :resources-item="resource"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+// local component
+import LearningResources from './LearningResources.vue';
+
+export default {
+  inject: ['resources'],
+  components: {
+    LearningResources,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
